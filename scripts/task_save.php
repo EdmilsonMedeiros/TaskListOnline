15159a60fce078de9f5502bd6255fb3700e5c3c6
EdmilsonMedeiros <edmilson.mprojetos@gmail.com>
2022-06-07 19:55:02 -0300

    Add tarefa

--- scripts/task_save.php
+++ scripts/task_save.php
@@ -1,5 +1,24 @@
 <?php 
-require_once('connect.php');
-
+session_start();
+$_titulo 		= $_POST['titulo'];
+$_dataTarefa 	= $_POST['dataTarefa'];
+$_conteudo 		= $_POST['conteudo'];
+$_emailSessao 	= $_SESSION['email'];
+$_querySelect	= "SELECT id FROM users WHERE email = '$_emailSessao'";
+if ($_emailSessao != null) {
+	include('connect.php');
+	$_consulta = mysqli_query($con, $_querySelect);
+	if (mysqli_num_rows($_consulta) != 0) {
+		$_lista = mysqli_fetch_array($_consulta);
+		$id = $_lista['id'];
+		$_queryInsert	= "INSERT INTO tarefas(titulo, dataTarefa, conteudo, id_user) VALUES('$_titulo', '$_dataTarefa', '$_conteudo', '$id')";
+		mysqli_query($con, $_queryInsert);
+		header("Location: ../home.php");
+	}else{
+		// header("Location: ../home.php");
+	}
+}else{
+	header("Location: ../");
+}
 
 ?>
